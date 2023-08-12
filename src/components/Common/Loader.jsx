694b604b1ab486e62938e85e71d2d66cec717da3
dab <dab@dab.com>
2023-08-12 14:58:45 +0300

    update

--- src/components/Common/Loader.jsx
+++ src/components/Common/Loader.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import cartIcon from '../../assets/images/icons/cart.svg'
 import {ReactSVG} from "react-svg";
-const Loader = (props) => {
+const Loader = () => {
     return (
         <div className="preloader">
             <ReactSVG className={'svg-icon'} src={cartIcon}/>
             <div className="preloader__text">
-                <p className="preloader__msg">Bringing you the goods…</p>
-                <p className="preloader__msg preloader__msg--last">This is taking long. Something’s wrong.</p>
+                <p className="preloader__msg">Идет загрузка…</p>
+                <p className="preloader__msg preloader__msg--last">Слишком долго загружается? Возможно что-то пошло не так.</p>
             </div>
         </div>
     );
